bd72eeef58c0f39bc6bd4cb5df54d4c9077133b1
Adriele Lima <adriele.p.t@hotmail.com>
2023-03-28 20:11:30 +0000

    Validating that all value are integers

--- run.py
+++ run.py
@@ -32,9 +32,8 @@ def validade_data(values):
     or if there aren't exactly 6 values.
     """
 
-    print(values)
-
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
